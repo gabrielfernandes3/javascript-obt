290de6a0238adfb78ae078c698a93816ae0903df
Gabriel Fernandes <125320440+gabrielfernandes3@users.noreply.github.com>
2023-09-27 21:20:37 -0300

    att

---
+++ aulas/aula012/index.js
@@ -0,0 +1,11 @@
+//Console
+let nome = "Gabriel"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//Window
+window.alert(nome)
+window.prompt("Insira o seu nome:")
+window.confirm("Confirma essa ação?")
