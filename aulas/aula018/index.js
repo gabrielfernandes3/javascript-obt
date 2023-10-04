4deaf7f12b0c790241a3b9f7d4226ccade254e2b
Gabriel Fernandes <125320440+gabrielfernandes3@users.noreply.github.com>
2023-10-03 23:19:19 -0300

    att

---
+++ aulas/aula018/index.js
@@ -0,0 +1,22 @@
+/* if (6 === 6) {
+  console.log("Verdadeiro")
+}
+
+if (5 !== 5) {
+  console.log("Verdadeiro")
+} else {
+  console.log("Falso")
+} */
+
+/*const idade = prompt("Informe a sua idade:")
+
+if (idade > 18) {
+  alert("Você é maior de idade")
+} else if (idade < 12){
+  alert("Você é menor de idade")
+} else if (idade > 4) {
+  alert("Você é criança")
+} */
+
+const resultado = (6 === 6) ? "Verdadeiro" : "Falso" 
+console.log(resultado)
